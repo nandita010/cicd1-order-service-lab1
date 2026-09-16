a72bf47eebea3a9faf71be55b3b454080d9ccd11
NanditaReji-STUDENT <g00463584@atu.ie>
2026-09-16 15:38:52 +0100

    order service created

---
+++ src/main/java/ie/atu/cicd1/catalog/cicd1orderservice1/Cicd1OrderService1Application.java
@@ -0,0 +1,13 @@
+package ie.atu.cicd1.catalog.cicd1orderservice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cicd1OrderService1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cicd1OrderService1Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/cicd1/catalog/cicd1orderservice1/Cicd1OrderService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.cicd1.catalog.cicd1orderservice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cicd1OrderService1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
